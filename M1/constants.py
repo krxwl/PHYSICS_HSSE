551f0dc967d60af428d8ddb6bfc323531b694658
Юлия Сысоева <sysoeva.iua@phystech.edu>
2026-09-17 04:05:28 +0300

    Some fixes and quad diffur

--- M1/constants.py
+++ M1/constants.py
@@ -1,5 +1,5 @@
-INPUT_ALPHA_STR: str = "Введите угол:\t"
-INPUT_INITIAL_SPEED_STR: str = "Введите начальную скорость v_0:\t"
+INPUT_ALPHA_STR: str = "Введите угол (в градусах):\t"
+INPUT_INITIAL_SPEED_STR: str = "Введите начальную скорость v_0 (в м/с):\t"
 INPUT_COEFFICIENT_STR: str =  "Введите коэффициент сопротивления:\t" 
 INPUT_WEIGHT_STR: str = "Введите вес тела (в кг): \t"
 
--- M1/main.py
+++ M1/main.py
@@ -1,27 +1,43 @@
-from numpy import ndarray, array # быстрый массивчик
+from numpy import ndarray, array  # быстрый массивчик
 
 from scipy.integrate import solve_ivp  # обертка решения диффура
 from scipy.constants import g
 
-from math import cos, sin, radians
+from math import cos, sin, radians, sqrt
 
 import matplotlib.pyplot as plt
-from matplotlib.ticker import MultipleLocator # для изменения масштаба
+from matplotlib.ticker import MultipleLocator  # для изменения масштаба
 
 from constants import *
 
 
+# функция, показывающая, когда камень упадет
+def hit_ground(t: float, coordinates: ndarray[float]) -> float:
+    """
+    функция которая когда тело упадет на землю (y=0) перестает
+    интегрировать диффур на остальном интервале
+    """
+    return coordinates[1]
+
+
+hit_ground.terminal = True
+hit_ground.direction = 1
+
+
 class Solver:
     def __init__(self):
-        self.alpha, \
-        self.v_0, \
-        self.resistance_coefficient, \
-        self.formula_choice_tag, \
-        self.weight = self.read_data()
+        (
+            self.alpha,
+            self.v_0,
+            self.resistance_coefficient,
+            self.formula_choice_tag,
+            self.weight,
+        ) = self.read_data()
 
         if self.formula_choice_tag is False:
             self.visualise_data(self.get_viscous_resistance_data())
-
+        else:
+            self.visualise_data(self.get_frontal_resistance_data())
 
     def read_data(self) -> tuple[float, float, float, bool, float]:
         """
@@ -36,7 +52,6 @@ class Solver:
 
         return tuple([alpha, v_0, resistance_coefficient, formula_choice_flag, weight])
 
-
     # coords - текущая точка в которой мы находимся
     def speed_equation(self, t: float, coordinates: ndarray[float]) -> list[float]:
         """
@@ -45,7 +60,7 @@ class Solver:
         x, y, v_x, v_y = coordinates
 
         # вычисляем производные в точке
-        dvx_dt = - self.resistance_coefficient / self.weight * v_x
+        dvx_dt = -self.resistance_coefficient / self.weight * v_x
         dvy_dt = -g - self.resistance_coefficient / self.weight * v_y
 
         dx_dt = v_x
@@ -53,43 +68,72 @@ class Solver:
 
         return array([dx_dt, dy_dt, dvx_dt, dvy_dt])
 
-
-    def hit_ground(self, t: float, coordinates: ndarray[float]) -> float:
+    def quad_speed_equation(self, t: float, coordinates: ndarray[float]) -> list[float]:
         """
-        функция которая когда тело упадет на землю (y=0) перестает
-        интегрировать диффур на остальном интервале
+        решаем диффур для вязкого трения
         """
-        return coordinates[1]
+        x, y, v_x, v_y = coordinates
 
+        # вычисляем производные в точке
+        dvx_dt = (
+            -self.resistance_coefficient / self.weight * v_x * sqrt(v_x**2 + v_y**2)
+        )
+        dvy_dt = -g - self.resistance_coefficient / self.weight * v_y * sqrt(
+            v_x**2 + v_y**2
+        )
+
+        dx_dt = v_x
+        dy_dt = v_y
+
+        return array([dx_dt, dy_dt, dvx_dt, dvy_dt])
+
+    def get_start_conditions(self) -> list[float]:
+        """
+        функция возвращает начальное положение тела
+        и проекции начальной скорости
+        """
+        x0 = 0.0
+        y0 = 0.00000001  # чтобы интегрирование не закончилось сразу же
+        v_x0 = self.v_0 * cos(radians(self.alpha))
+        v_y0 = self.v_0 * sin(radians(self.alpha))
+        return array([x0, y0, v_x0, v_y0])
 
     def get_viscous_resistance_data(self) -> ndarray[float]:
         # решаем задачу коши
-        result = solve_ivp(fun=self.speed_equation, 
-                                      t_span=(0, SPAN_MAX_UPPER_BORDER), # область интегрирования
-                                      y0 = (0.0, 0.0, self.v_0 * cos(radians(self.alpha)), self.v_0 * sin(radians(self.alpha))), # начальные условия (x0, y0, v_x0, v_y0)
-                                      events=self.hit_ground,  #  перестанет вычислять диффур на остальной области определения после того как событие произойдет
-                                      terminal=True, #  прерваться на падении
-                                      dense_output=True # решение непрерывно
-                                      )
+        result = solve_ivp(
+            fun=self.speed_equation,
+            t_span=(0, SPAN_MAX_UPPER_BORDER),  # область интегрирования
+            y0=self.get_start_conditions(),  # начальные условия (x0, y0, v_x0, v_y0)
+            events=hit_ground,  #  перестанет вычислять диффур на остальной области определения после того как событие произойдет
+            dense_output=True,  # решение непрерывно
+        )
         return result.y
-        
+
+    def get_frontal_resistance_data(self) -> ndarray[float]:
+        # решаем задачу коши
+        result = solve_ivp(
+            fun=self.quad_speed_equation,
+            t_span=(0, SPAN_MAX_UPPER_BORDER),  # область интегрирования
+            y0=self.get_start_conditions(),  # начальные условия (x0, y0, v_x0, v_y0)
+            events=hit_ground,  #  перестанет вычислять диффур на остальной области определения после того как событие произойдет
+            dense_output=True,  # решение непрерывно
+        )
+        return result.y
 
     def visualise_data(self, data: ndarray[float]) -> None:
         """
         построит график на данных
         """
         #  axes - все элементы графика, figure - контейнер верхнего уровня для графика
-        figure, axes = plt.subplots(figsize=(20, 20)) # размер побольше
+        figure, axes = plt.subplots(figsize=(20, 20))  # размер побольше
         axes.plot(data[0][::10], data[1][::10])  # делаем оси x и y
-        axes.grid() # бьем на клетки
+        axes.grid()  # бьем на клетки
 
         axes.set_xlim(left=0)
         axes.set_ylim(bottom=0, top=20)
-        axes.plot(data[0], data[1], color='red', linewidth=5, label="Траектория")
-        
-        plt.show() # показать график
+        axes.plot(data[0], data[1], color="red", linewidth=5, label="Траектория")
 
-
+        plt.show()  # показать график
 
 
 if __name__ == "__main__":
